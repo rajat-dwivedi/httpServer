c3ae184103273718a0acf48458e6333b52761070
rajat-dwivedi <rdroid302@gmail.com>
2025-06-07 13:34:50 -0500

    3 basic socket and http response (single connection)

--- src/main/java/com/working/httpServer/HttpServer.java
+++ src/main/java/com/working/httpServer/HttpServer.java
@@ -3,6 +3,13 @@ package com.working.httpServer;
 import com.working.httpServer.config.ConfigManager;
 import com.working.httpServer.config.Configration;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+
 public class HttpServer {
     public static void main(String[] args) {
         System.out.println("starting server");
@@ -11,5 +18,37 @@ public class HttpServer {
 
         System.out.println("Current port "+conf.getPort());
         System.out.println("Current webRoot "+conf.getWebroot());
+
+        try {
+            ServerSocket serverSocket = new ServerSocket(conf.getPort());
+            Socket socket = serverSocket.accept();
+
+            InputStream inputStream = socket.getInputStream();
+            OutputStream outputStream = socket.getOutputStream();
+
+            //we would read
+
+            //we would write
+
+            // HTML content
+            String html = "<html><body><h1>Hello, World! This is the initial hard coded dumb output</h1></body></html>";
+            final String CRLF = "\r\n"; // Line separator in HTTP
+            // Build HTTP response
+            String response =
+                    "HTTP/1.1 200 OK" + CRLF +
+                            "Content-Type: text/html; charset=UTF-8" + CRLF +
+                            "Content-Length: " + html.length() + CRLF +
+                            CRLF +
+                            html;
+
+            outputStream.write(response.getBytes());
+
+            inputStream.close();
+            outputStream.close();
+            socket.close();
+            serverSocket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
